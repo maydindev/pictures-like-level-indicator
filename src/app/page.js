b2a8f974f70cf91b2aa27f51e2e4ace6e4b6380f
maydindev <maydin.dev@outlook.com>
2024-07-06 10:39:00 +0300

    Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,78 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [images, setImages] = useState([]);
+
+  // Resim eklemek için fonksiyon
+  const handleAddImage = () => {
+    const newImageUrl = `https://picsum.photos/seed/${crypto.randomUUID()}/200/300`;
+    setImages([...images, { url: newImageUrl, likeCount: 0, sizeStep: 0 }]);
+  };
+
+  // Like sayısını artırmak için fonksiyon
+  const handleLike = (index) => {
+    const updatedImages = [...images];
+    updatedImages[index].sizeStep = (updatedImages[index].sizeStep + 1) % 3;
+    setImages(updatedImages);
+  };
+
+  // Emoji boyutunu değiştirmek için fonksiyon
+  const getEmojiSize = (sizeStep) => {
+    switch (sizeStep) {
+      case 0:
+        return "2rem";
+      case 1:
+        return "4rem";
+      case 2:
+        return "6rem";
+      default:
+        return "2rem";
+    }
+  };
+
+  const emojiStyle = {
+    fontSize: "2rem",
+    transition: "font-size 0.3s ease",
+    textDecoration: "none",
+    cursor: "pointer",
+  };
+
+  const buttonStyle = {
+    backgroundColor: "white",
+    fontSize: 16,
+    color: "black",
+    padding: "15px",
+    margin: "15px",
+    borderRadius: "10px",
+    width: "150px",
+    cursor: "pointer",
+  }
+
+  return (
+    <>
+      <button style={buttonStyle} onClick={handleAddImage}>Resim Ekle</button>
+      <div>
+        {images.map((image, index) => (
+          <span key={index}>
+            <input type="image" alt="LoremPicsum" src={image.url} />
+            <a href="#" onClick={() => handleLike(index)} style={{ ...emojiStyle, fontSize: getEmojiSize(image.sizeStep) }}>
+              👍🏻
+            </a>
+          </span>
+        ))}
+      </div>
+    </>
+  );
+}
+
+
+
+//new nextjs file template code
+/*
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -91,5 +162,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  );
-}
+  )
+    */
